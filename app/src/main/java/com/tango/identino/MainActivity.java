644e8a7194daa55db118104835ce52fbd15816ca
Git-Uzair <uzairahmed.x@gmail.com>
2019-11-30 19:39:55 +0500

    Login screen to Post login Activity to test

--- app/src/main/java/com/tango/identino/MainActivity.java
+++ app/src/main/java/com/tango/identino/MainActivity.java
@@ -2,13 +2,28 @@ package com.tango.identino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginbtn = findViewById(R.id.login_button);
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Post_login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/tango/identino/Post_login.java
@@ -0,0 +1,14 @@
+package com.tango.identino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Post_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_login);
+    }
+}
