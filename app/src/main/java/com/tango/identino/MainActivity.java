85d0a5c09286ca6384460146b95cb36444c34aa5
Git-Uzair <uzairahmed.x@gmail.com>
2019-11-30 20:43:21 +0500

    Username - > Email
    Firebase login functionality

--- app/src/main/java/com/tango/identino/MainActivity.java
+++ app/src/main/java/com/tango/identino/MainActivity.java
@@ -1,29 +1,70 @@
 package com.tango.identino;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button loginbtn;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private EditText Email, Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
         loginbtn = findViewById(R.id.login_button);
+        Email = findViewById(R.id.login_email_edit_text);
+        Password = findViewById(R.id.login_password_edit_text);
+
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Post_login.class);
-                startActivity(intent);
-                finish();
+
+                if (Email.getText().toString().isEmpty() || Password.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please fill in all the fields", Toast.LENGTH_LONG).show();
+                } else {
+                    mAuth.signInWithEmailAndPassword(Email.getText().toString(), Password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(getApplicationContext(), Post_login.class);
+                                startActivity(intent);
+                                finish();
+                            }
+
+                        }
+                    });
+                }
+
+
+//
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = mAuth.getCurrentUser();
+
     }
 }
