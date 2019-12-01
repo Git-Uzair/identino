cf2e60583ff903e4cf579bccb10b1a9bb1ae85b8
Gulfam <gulfamhussain80@gmail.com>
2019-12-01 18:39:40 +0500

    display function is made. courses are displayed from database in recycler view.

--- app/src/main/java/com/tango/identino/Post_login.java
+++ app/src/main/java/com/tango/identino/Post_login.java
@@ -1,5 +1,6 @@
 package com.tango.identino;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,6 +9,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.tango.identino.model.courses;
 import com.tango.identino.util.courseAdapter;
 
 import java.util.ArrayList;
@@ -18,6 +26,9 @@ public class Post_login extends AppCompatActivity {
     private com.tango.identino.util.courseAdapter courseAdapter;
     private List<String> courseList;
     private TextView Ins_name;
+    private FirebaseFirestore db=FirebaseFirestore.getInstance();
+    private FirebaseAuth mauth;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +38,34 @@ public class Post_login extends AppCompatActivity {
         Ins_name = findViewById(R.id.post_login_username_textView);
         Ins_name.setText(getIntent().getStringExtra("name"));
         courseList=new ArrayList<>();
-
+        mauth=FirebaseAuth.getInstance();
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        currentUser=mauth.getCurrentUser();
+        displayCourses();
 
-        courseAdapter=new courseAdapter(courseList,Post_login.this);
-        recyclerView.setAdapter(courseAdapter);
+
+    }
+
+    private void displayCourses()
+    {
+        String email=currentUser.getEmail();
+        db.collection("instructor").document(email).collection("timetable").document("monday")
+                .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                courses Courses=documentSnapshot.toObject(courses.class);
+                List<String> coursesList=Courses.getCourses();
+                courseAdapter=new courseAdapter(coursesList,Post_login.this);
+                recyclerView.setAdapter(courseAdapter);
 
 
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
     }
 }
