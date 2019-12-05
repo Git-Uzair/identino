4e85c20f65d3b3428fae5b62bbbdeaba06016a81
Gulfam <gulfamhussain80@gmail.com>
2019-12-06 00:21:26 +0500

    camera opens.

--- app/src/main/java/com/tango/identino/TakePhoto.java
+++ app/src/main/java/com/tango/identino/TakePhoto.java
@@ -1,14 +1,45 @@
 package com.tango.identino;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class TakePhoto extends AppCompatActivity {
+import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.controls.Facing;
+import com.otaliastudios.cameraview.frame.Frame;
+import com.otaliastudios.cameraview.frame.FrameProcessor;
+import com.theartofdev.edmodo.cropper.CropImage;
 
+public class TakePhoto extends AppCompatActivity implements FrameProcessor {
+
+
+    private ImageView imageView;
+    private ImageButton cameraButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_photo);
+
+        imageView=findViewById(R.id.take_photo_image_view);
+        cameraButton=findViewById(R.id.take_photo_camera_button);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    @Override
+    public void process(@NonNull Frame frame) {
+
     }
 }
