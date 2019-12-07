84fa6097d91e56a2893dc861117d5611f9063ed2
Gulfam <gulfamhussain80@gmail.com>
2019-12-08 04:23:05 +0500

    Takes image successfully from both front and back camera. And displays the image successfully in the image view.

--- app/src/main/java/com/tango/identino/TakePhoto.java
+++ app/src/main/java/com/tango/identino/TakePhoto.java
@@ -1,51 +1,44 @@
 package com.tango.identino;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-import com.otaliastudios.cameraview.CameraView;
-import com.otaliastudios.cameraview.controls.Facing;
-import com.otaliastudios.cameraview.frame.Frame;
-import com.otaliastudios.cameraview.frame.FrameProcessor;
-import com.theartofdev.edmodo.cropper.CropImage;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class TakePhoto extends AppCompatActivity {
-
-
-    private ImageView imageView;
     private ImageButton cameraButton;
+    private ImageView imageView;
+    private static final int CAMERA_PIC_REQUEST = 1337;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_photo);
-
-        imageView=findViewById(R.id.take_photo_image_view);
-        cameraButton=findViewById(R.id.take_photo_camera_button);
-
+        cameraButton = findViewById(R.id.take_photo_camera_button);
+        imageView = findViewById(R.id.take_photo_image_view);
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,0);
-
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
             }
         });
+
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap= (Bitmap) data.getExtras().get("data");
-        imageView.setImageBitmap(bitmap);
-
+        if (requestCode == CAMERA_PIC_REQUEST) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(image);
+        }
     }
 }
